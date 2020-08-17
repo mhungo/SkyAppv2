f9740c1a7b7ee28eec8903f8d2f95ee26b448c6e
mhungo <phamhung2015cb@gmail.com>
2020-08-17 16:47:16 +0700

    Add call

---
+++ app/src/main/java/com/example/skyapp/VideoChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.skyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+    }
+}
